42ad903f05551af1720b5b962f6dda7d15dad65b
SUPHAKKHAWII6812022610298 <S6812022610298@email.kmutnb.ac.th>
2025-07-14 17:36:24 +0700

    compro w04 wave 6812022610298

---
+++ Coding_w04-01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+    int main(){                 // int Keyword // main Identifier
+        printf("Hello \n");     //printf Identifier 
+        printf("C Tokens!\n");  //printf Identifier 
+        return 0;               // return Keyword
+
+    }
---
+++ Coding_w04-03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char nameInitial = 'S';  
+int age = 20;   
+float weight = 76.5;
+char gender[] = "male"; 
+
+printf("nameInitial: %c\n", nameInitial ); 
+printf("Age: %d\n", age );            
+printf("Weight: %.1f kg\n", weight );  
+printf("Gender: %s\n", gender);
+ return 0 ;  
+}
+
